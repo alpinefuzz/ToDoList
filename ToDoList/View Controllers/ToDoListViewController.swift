85300e52b7c7ba833d590fb0aafef0301815caee
Vitaliy Kurbatov <vitaliykurbatov@macbook-pro-vitaliy.local>
2018-03-15 05:36:34 +0300

    Persisting data using Codable and plist

--- ToDoList/View Controllers/ToDoListViewController.swift
+++ ToDoList/View Controllers/ToDoListViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
+    
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-let newItem = Item()
-        
+        loadItems()
     }
-// table view data source methods
+    
+    
+    // table view data source methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-
+        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
@@ -41,21 +41,20 @@ let newItem = Item()
         
         return cell
     }
-
-//    tableView Delegate methods
+    
+    //    tableView Delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        
-//  making opposite
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-    
-        tableView.reloadData()
+        
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
+    //    Add New Items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
@@ -69,9 +68,8 @@ let newItem = Item()
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            self.saveItems()
             
-            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -84,6 +82,33 @@ let newItem = Item()
         
         present(alert, animated: true, completion: nil)
     }
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("\(error)")
+            }
+        }
+    }
+    
+    
     
     
 }
